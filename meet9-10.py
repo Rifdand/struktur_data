d3d0452c9ad344bfdbbce8e0c33de1ba1b550c05
Rifdand <rifdand919@gmail.com>
2025-07-04 19:37:22 +0800

    Delete meet9-10.py

--- meet9-10.py
+++
@@ -1,7 +0,0 @@
-#meminta input
-domain = input("Masukkan nama domain anda: ")
-#memsihkkan antara titik
-nama, ekstensi = domain.split(".")
-# Menampilkan 
-print("Nama domain anda adalah =", nama)
-print("Nama yang anda gunakan adalah = ", ekstensi)
